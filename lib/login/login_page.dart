a6355ff445118b37bf898b38f24c4d7e904714af
artnest <artem291997@gmail.com>
2019-01-09 02:50:06 +0300

    Update Login page text fields

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        padding: const EdgeInsets.symmetric(horizontal: 32),
-        color: Colors.blue[400],
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.symmetric(horizontal: 32),
           children: <Widget>[
             TextField(
               decoration: const InputDecoration(
                 border: InputBorder.none,
-                hintText: 'Login',
+                hintText: 'E-mail',
+                contentPadding: const EdgeInsets.all(16),
               ),
+              controller: emailController,
             ),
             TextField(
               decoration: const InputDecoration(
                 border: InputBorder.none,
                 hintText: 'Password',
+                contentPadding: const EdgeInsets.all(16),
               ),
+              controller: passwordController,
             )
           ],
         ),
